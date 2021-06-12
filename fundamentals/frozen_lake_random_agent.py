69e121225c71b0c77e091278a4dc4eeee7d60f11
Indy Navarro <indy.mnv@gmail.com>
2021-06-12 19:47:10 -0400

    frozen lake scenario with viz

--- fundamentals/frozen_lake_random_agent.py
+++ fundamentals/frozen_lake_random_agent.py
@@ -1,8 +1,27 @@
 import gym
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
 
+games = 1000
+scores = []
+win_pct = []
 env = gym.make('FrozenLake-v0')
 env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
-env.close()
+for i in range(games):
+    done = False
+    obs = env.reset()
+    score = 0
+
+    while not done:
+        action = env.action_space.sample()
+        obs, reward, done, info  = env.step(action)
+        score += reward
+    scores.append(score)
+
+    if i % 10 == 0:
+        average = np.mean(scores[-10:])
+        win_pct.append(average)
+
+plt.plot(win_pct)
+plt.show()
