f0a673a24d76be37ec1f4710628810ad93459b88
Indy Navarro <indy.mnv@gmail.com>
2021-06-23 07:24:36 -0400

    inserting model

--- pytorch/exercise.py
+++ pytorch/exercise.py
@@ -30,3 +30,29 @@ for X, y in test_dataloader:
     break
 
 
+#Creacion de los modelos
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("Using {} device".format(device))
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self). __init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
+
